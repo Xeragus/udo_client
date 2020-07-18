aca552c7d1bd5735026880375b91e9da9059cf6f
Boban Sugareski <bobansugareski@Boban-Sugareski-MacBook-Pro.local>
2020-07-19 01:18:35 +0200

    Add initial react app files, router and basic component structure

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Dashboard = () => {
+  return (
+    <div>
+      <div>
+        <h1>Dashboard</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
